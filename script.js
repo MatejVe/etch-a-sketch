5b0ba038b45e9d665ff602e19313ea3821f4e938
Matej <matejvedak@gmail.com>
2022-05-28 16:40:45 +0200

    Create the grid resize button and the corresponding method
    The main functionality of the website works as indentend.

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const gridDiv = document.querySelector('.grid');
 
-
-for (var i = 0; i < 16; i++) {
-    var rowBox = document.createElement('div');
-    rowBox.classList.add("rowBox");
-
-    for (var j = 0; j < 16; j++) {
-        var box = document.createElement('div');
-        box.classList.add('box');
-        rowBox.appendChild(box)
-    }
-
-    gridDiv.appendChild(rowBox);
+const createGrid = function(gridDimension=16) {
+    for (var i = 0; i < gridDimension; i++) {
+        var rowBox = document.createElement('div');
+        rowBox.classList.add("rowBox");
+    
+        for (var j = 0; j < gridDimension; j++) {
+            var box = document.createElement('div');
+            box.classList.add('box');
+            rowBox.appendChild(box);
+        }
+    
+        gridDiv.appendChild(rowBox);
+    };
+    
+    document.querySelectorAll('.box').forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            item.setAttribute('style', 'background-color:black;');
+        });
+    });   
 };
 
-document.querySelectorAll('.box').forEach(item => {
-    item.addEventListener('mouseenter', event => {
-        item.setAttribute('style', 'background-color:black;');
-    });
+createGrid();
+
+const changeSizeBtn = document.querySelector('#gridSize');
+
+changeSizeBtn.addEventListener('click', () => {
+    let validNumber = false;
+
+    while (!validNumber) {
+        var newSize = prompt("Please enter the size of the grid");
+        if (newSize < 101 && newSize > 0) {
+            validNumber = true;
+        };
+    };
+
+    while (gridDiv.firstElementChild) {
+        gridDiv.firstElementChild.remove();
+    };
+
+    createGrid(newSize);
 });
